714ea7d99bbea25b784f2d4ef5dc43691f5b0b0f
Kaleb Mesfin <kalemesfin12@gmail.com>
2024-04-05 11:28:31 +0300

    forogotten nullable

--- backend/src/product/model/product.model.ts
+++ backend/src/product/model/product.model.ts
@@ -3,8 +3,8 @@ import { BaseModel } from 'src/common/models/base.model';
 
 @ObjectType()
 export class Product extends BaseModel {
-  @Field()
-  name: string;
+  @Field({ nullable: true })
+  name?: string;
 
   @Field({ nullable: true })
   description?: string;
